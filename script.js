a6526bb1eac487db709bfa14b38aae108a49a66b
rifqihanapi03 <rifqihanapi4@gmail.com>
2022-06-12 16:43:29 +0700

    menambahkan semua fungsi

--- script.js
+++ script.js
@@ -1,11 +1,99 @@
-const calculatorScreen = document.querySelector(".calculator-screen");
+const decimal = document.querySelector('.decimal');
+
+decimal.addEventListener('click', (event) => {
+  inputDecimal(event.target.value);
+  updateScreen(currentNumber);
+})
+
+inputDecimal = (dot) => {
+  if (currentNumber.includes('.')) {
+    return;
+  }
+  currentNumber += dot;
+}
+
+const clearBtn = document.querySelector('.all-clear');
+
+clearBtn.addEventListener('click', () => {
+  clearAll();
+  updateScreen(currentNumber);
+})
+
+const clearAll = () => {
+  prevNumber = ''
+  calculatorOperator = ''
+  currentNumber = '0'
+}
+
+const calculator = () => {
+  let result = ''
+  switch(calculatorOperator) {
+    case "+":
+      result = parseFloat (prevNumber) + parseFloat (currentNumber)
+      break;
+    case "-":
+      result = parseFloat (prevNumber) + parseFloat (currentNumber)
+      break;
+    case "*":
+      result = parseFloat (prevNumber) + parseFloat (currentNumber)
+      break;
+    case "/":
+      result = parseFloat (prevNumber) + parseFloat (currentNumber)
+      break;
+    default:
+      return;
+  }
+  currentNumber = result;
+  calculatorOperator = '';
+}
+
+const equalSign = document.querySelectorAll('.equal-sign');
+
+equalSign.addEventListener('click', () => {
+  calculator();
+  updateScreen(currentNumber);
+})
+
+const inputOperator = (operator) => {
+  if (calculatorOperator === '') {
+    prevNumber = currentNumber
+  }
+  calculatorOperator = operator
+  currentNumber = '0'
+}
+
+const operators = document.querySelectorAll('.operator');
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", (event) => {
+    inputOperator(event.target.value);
+  })
+})
+
+
+let prevNumber = '';
+let currentNumber = '';
+let calculatorOperator = '0';
+
+const inputNumber = (number) => {
+  if (currentNumber === '0') {
+    currentNumber = number;
+  } else {
+    currentNumber += number;
+  }
+};
+
+const calculatorScreen = document.querySelector('.calculator-screen');
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (event) => {
+    inputNumber(event.target.value);
+    updateScreen(event.target.value);
+  });
+});
 
 const updateScreen = (number) => {
   calculatorScreen.value = number;
-};
-
-const inputNumber = (number) => {
-  currentNumber = number;
 };
 
 numbers.forEach((number) => {
@@ -14,7 +102,7 @@ numbers.forEach((number) => {
   });
 });
 
-const numbers = document.querySelectorAll(".number");
+const numbers = document.querySelectorAll('.number');
 
 numbers.forEach((numbers) => {
   numbers.addEventListener("click", (event) => {
